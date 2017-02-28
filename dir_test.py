387ea16f520f16bfdc9e90ddac77197546866f32
extreems <sgoubar@yahoo.com>
2017-02-28 19:34:56 +0300

    PEP8 changes

--- dir_test.py
+++ dir_test.py
@@ -1,4 +1,18 @@
-import os    # Import the OS module
+# Script Name		:               dir_test.py
+# Author		:               Craig Richards
+# Created		:               29th November 2011
+# Last Modified		:
+# Version		:               1.0
+# Modifications		:
 
-if not os.path.exists('testdir'):  # Check to see if it exists
-    os.makedirs('testdir')  # Create the directory
+# Description		:               Tests to see if the directory testdir /
+#                                       exists, if not it will create the /
+#                                       directory for you
+
+
+
+
+import os                               # Import the OS module
+
+if not os.path.exists('testdir'):       # Check to see if it exists
+    os.makedirs('testdir')              # Create the directory
